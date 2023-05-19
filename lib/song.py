df298a595fc6cc71d8f0edf09b47b637cc712cac
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-19 16:42:59 +0300

    Done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,85 @@
+
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
+
+ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+print(ninety_nine_problems.name)
+print(ninety_nine_problems.artist)
+print(ninety_nine_problems.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+sikutambui = Song("Sikutambui", "Wakadinali", "Kenyan Drill")
+print(sikutambui.name)
+print(sikutambui.artist)
+print(sikutambui.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+clash = Song("Clash", "Dave", "UK Drill")
+print(clash.name)
+print(clash.artist)
+print(clash.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+do_for_love = Song("Do For Love", "2-pac", "Rap")
+print(do_for_love.name)
+print(do_for_love.artist)
+print(do_for_love.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
